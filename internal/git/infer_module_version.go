3873bed6537f464f33df6c341f5e5b0d7317b16d
Max Ignatenko <gelraen.ua@gmail.com>
2025-10-05 22:31:35 +0100

    fix: return only one tag when `git tag` returns multiple
    This makes version inference work correctly on github.com/kubernetes/api,
    returning just "v0.34.1" instead of "kubernetes-1.34.1\nv0.34.1".

--- internal/git/infer_module_version.go
+++ internal/git/infer_module_version.go
@@ -2,6 +2,9 @@ package git
 
 import (
 	"fmt"
+	"strings"
+
+	"golang.org/x/mod/semver"
 
 	"github.com/sourcegraph/scip-go/internal/command"
 )
@@ -10,12 +13,20 @@ import (
 // directory. This will be either the work tree commit's tag, or it will be the
 // short revhash of the HEAD commit.
 func InferModuleVersion(dir string) (string, error) {
-	version, err := command.Run(dir, "git", "tag", "-l", "--points-at", "HEAD")
+	tags, err := command.Run(dir, "git", "tag", "-l", "--points-at", "HEAD")
 	if err != nil {
-		return "", fmt.Errorf("failed to tags for current commit: %v\n%s", err, version)
+		return "", fmt.Errorf("failed to tags for current commit: %v\n%s", err, tags)
 	}
-	if version != "" {
-		return version, nil
+	if tags != "" {
+		lines := strings.Split(tags, "\n")
+		for _, tag := range lines {
+			if semver.IsValid(tag) {
+				return tag, nil
+			}
+		}
+
+		// None of the tags look like a version, but return one of them anyway.
+		return lines[0], nil
 	}
 
 	commit, err := command.Run(dir, "git", "rev-parse", "HEAD")
