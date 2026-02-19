c1b679b1bd595a4c8d0f8879fe7ba072c1106e17
jupblb <michal.kielbowicz@sourcegraph.com>
2026-02-19 13:39:33 +0100

    Regenerate snapshots

--- internal/testdata/snapshots/output/package-documentation/secondary.go
+++ internal/testdata/snapshots/output/package-documentation/secondary.go
@@ -1,12 +1,12 @@
   package packagedocumentation
-//        ^^^^^^^^^^^^^^^^^^^^ reference github.com/sourcegraph/scip-go . `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/
+//        ^^^^^^^^^^^^^^^^^^^^ reference github.com/sourcegraph/scip-go 0.1.test `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/
   
-//⌄ enclosing_range_start github.com/sourcegraph/scip-go . `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/AlsoExporter().
+//⌄ enclosing_range_start github.com/sourcegraph/scip-go 0.1.test `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/AlsoExporter().
   func AlsoExporter() {}
-//     ^^^^^^^^^^^^ definition github.com/sourcegraph/scip-go . `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/AlsoExporter().
+//     ^^^^^^^^^^^^ definition github.com/sourcegraph/scip-go 0.1.test `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/AlsoExporter().
 //     documentation
 //     > ```go
 //     > func AlsoExporter()
 //     > ```
-//                     ⌃ enclosing_range_end github.com/sourcegraph/scip-go . `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/AlsoExporter().
+//                     ⌃ enclosing_range_end github.com/sourcegraph/scip-go 0.1.test `github.com/sourcegraph/scip-go/internal/testdata/snapshots/input/package-documentation`/AlsoExporter().
   
