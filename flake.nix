95fa5924cfc6f5263f7082b169721b517843c577
jupblb <michal.kielbowicz@sourcegraph.com>
2026-03-27 15:07:51 +0100

    Replace goreleaser with Nix cross-compilation

--- flake.nix
+++ flake.nix
@@ -19,15 +19,24 @@
       let
         pkgs = import nixpkgs { inherit system; };
         version = pkgs.lib.fileContents ./internal/index/version.txt;
-      in
-      {
-        packages = {
-          scip-go = pkgs.buildGoModule {
+
+        mkScipGo =
+          {
+            GOOS ? null,
+            GOARCH ? null,
+            doCheck ? true,
+          }:
+          pkgs.buildGoModule {
             pname = "scip-go";
             inherit version;
             src = ./.;
             vendorHash = "sha256-AqJ9tVDlSMiT/uPI0K0OliE2mTsFl6bwp1fS7w+PfLU=";
             subPackages = [ "cmd/scip-go" ];
+            env = {
+              CGO_ENABLED = 0;
+            } // pkgs.lib.optionalAttrs (GOOS != null) { inherit GOOS; }
+              // pkgs.lib.optionalAttrs (GOARCH != null) { inherit GOARCH; };
+            inherit doCheck;
             checkPhase = "go test ./...";
             # Use proxyVendor so deps go into the module cache instead
             # of a vendor/ directory that conflicts with test sub-modules
@@ -35,7 +44,42 @@
             proxyVendor = true;
             nativeCheckInputs = [ pkgs.git ];
           };
+      in
+      {
+        packages = {
+          scip-go = mkScipGo { };
           default = self.packages.${system}.scip-go;
+
+          scip-go-linux-amd64 = mkScipGo {
+            GOOS = "linux";
+            GOARCH = "amd64";
+            doCheck = false;
+          };
+          scip-go-linux-arm64 = mkScipGo {
+            GOOS = "linux";
+            GOARCH = "arm64";
+            doCheck = false;
+          };
+          scip-go-darwin-amd64 = mkScipGo {
+            GOOS = "darwin";
+            GOARCH = "amd64";
+            doCheck = false;
+          };
+          scip-go-darwin-arm64 = mkScipGo {
+            GOOS = "darwin";
+            GOARCH = "arm64";
+            doCheck = false;
+          };
+          scip-go-windows-amd64 = mkScipGo {
+            GOOS = "windows";
+            GOARCH = "amd64";
+            doCheck = false;
+          };
+          scip-go-windows-arm64 = mkScipGo {
+            GOOS = "windows";
+            GOARCH = "arm64";
+            doCheck = false;
+          };
 
           docker = pkgs.dockerTools.buildLayeredImage {
             name = "scip-go";
