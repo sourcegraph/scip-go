5638dd4862b27175247bcc44ebc67246db24aaa0
jupblb <michal.kielbowicz@sourcegraph.com>
2026-04-14 23:14:00 +0200

    Use unique method in I3 interface to avoid platform-dependent stdlib matches
    Replace Close() error with ScipTestMethod() so the embedded
    implementation test doesn't pull in dozens of stdlib types that
    vary across OS/arch. The pr198 test still covers external symbol
    emission with a controlled dependency.

--- internal/testdata/snapshots/output/testdata/implementations_embedded.go
+++ internal/testdata/snapshots/output/testdata/implementations_embedded.go
@@ -1,30 +1,38 @@
   package testdata
 //        ^^^^^^^^ definition 0.1.test `sg/testdata`/
   
-  import "io"
-//        ^^ reference github.com/golang/go/src go1.22 io/
-  
   type I3 interface {
 //     ^^ definition 0.1.test `sg/testdata`/I3#
 //        signature_documentation
-//        > type I3 interface{ Close() error }
-   Close() error
-// ^^^^^ definition 0.1.test `sg/testdata`/I3#Close.
-//       signature_documentation
-//       > func (I3).Close() error
+//        > type I3 interface{ ScipTestMethod() }
+   ScipTestMethod()
+// ^^^^^^^^^^^^^^ definition 0.1.test `sg/testdata`/I3#ScipTestMethod.
+//                signature_documentation
+//                > func (I3).ScipTestMethod()
+  }
+  
+  type EmbeddedI3 interface {
+//     ^^^^^^^^^^ definition 0.1.test `sg/testdata`/EmbeddedI3#
+//                signature_documentation
+//                > type EmbeddedI3 interface{ ScipTestMethod() }
+   ScipTestMethod()
+// ^^^^^^^^^^^^^^ definition 0.1.test `sg/testdata`/EmbeddedI3#ScipTestMethod.
+//                signature_documentation
+//                > func (EmbeddedI3).ScipTestMethod()
+//                relationship 0.1.test `sg/testdata`/EmbeddedI3#ScipTestMethod. implementation
+//                relationship 0.1.test `sg/testdata`/I3#ScipTestMethod. implementation
   }
   
   type TClose struct {
 //     ^^^^^^ definition 0.1.test `sg/testdata`/TClose#
 //            signature_documentation
-//            > type TClose struct{ Closer }
-//            relationship github.com/golang/go/src go1.22 io/Closer# implementation
+//            > type TClose struct{ EmbeddedI3 }
+//            relationship 0.1.test `sg/testdata`/EmbeddedI3# implementation
 //            relationship 0.1.test `sg/testdata`/I3# implementation
-   io.Closer
-// ^^ reference github.com/golang/go/src go1.22 io/
-//    ^^^^^^ definition 0.1.test `sg/testdata`/TClose#Closer.
-//           signature_documentation
-//           > struct field Closer io.Closer
-//    ^^^^^^ reference github.com/golang/go/src go1.22 io/Closer#
+   EmbeddedI3
+// ^^^^^^^^^^ definition 0.1.test `sg/testdata`/TClose#EmbeddedI3.
+//            signature_documentation
+//            > struct field EmbeddedI3 sg/testdata.EmbeddedI3
+// ^^^^^^^^^^ reference 0.1.test `sg/testdata`/EmbeddedI3#
   }
   
