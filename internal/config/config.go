9ec8ca4d65ec684a63c0586731958335c0fd8253
jupblb <michal.kielbowicz@sourcegraph.com>
2026-04-20 14:29:56 +0200

    Populate ToolInfo.Arguments and remove config.New
    - Record os.Args[1:] in ToolInfo.Arguments metadata instead of an empty slice
    - Remove config.New; construct IndexOpts directly in makeOptions
    - Add Arguments field to IndexOpts

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,10 +1,5 @@
 package config
 
-import (
-	"os"
-	"path/filepath"
-)
-
 type IndexOpts struct {
 	ModuleRoot    string
 	ModuleVersion string
@@ -25,26 +20,7 @@ type IndexOpts struct {
 
 	// Package patterns to index
 	PackagePatterns []string
-}
 
-func New(ModuleRoot, ModuleVersion, ModulePath, GoStdlibVersion string, IsIndexingStdlib bool, SkipImplementations bool, SkipTests bool, PackagePatterns []string) IndexOpts {
-	ModuleRoot, err := filepath.Abs(ModuleRoot)
-	if err != nil {
-		panic(err)
-	}
-
-	driver := os.Getenv("GOPACKAGESDRIVER")
-	isGoPackagesDriverSet := driver != "" && driver != "off"
-
-	return IndexOpts{
-		ModuleRoot:            ModuleRoot,
-		ModuleVersion:         ModuleVersion,
-		ModulePath:            ModulePath,
-		GoStdlibVersion:       GoStdlibVersion,
-		SkipImplementations:   SkipImplementations,
-		SkipTests:             SkipTests,
-		IsIndexingStdlib:      IsIndexingStdlib,
-		PackagePatterns:       PackagePatterns,
-		IsGoPackagesDriverSet: isGoPackagesDriverSet,
-	}
+	// Arguments passed to the CLI
+	Arguments []string
 }
